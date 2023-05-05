27478d4546e6b16ec0d69eb66efc419a30ce031b
Sakar Bhandari <bhandarisakar7@gmail.com>
2023-05-06 00:05:12 +0545

    Delete Done

--- sakar/sakarapp/views.py
+++ sakar/sakarapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .forms import StudentRegistration
 from .models import User
+
+# This function will add and show data
 def add_show(request):
     if request.method == 'POST':
         fm=StudentRegistration(request.POST)
@@ -12,3 +14,9 @@ def add_show(request):
         fm = StudentRegistration()
     info = User.objects.all()
     return render(request, 'sakarapp/addandshow.html' , {'form':fm , 'info':info})
+# this function will delete data
+def delete_data(request , id):
+    if request.method== 'POST':
+        pi=User.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect('/')
