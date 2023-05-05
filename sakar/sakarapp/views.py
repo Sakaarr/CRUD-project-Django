8de03fb548ff88eb689b65c1973631e17f32b2a5
Sakar Bhandari <bhandarisakar7@gmail.com>
2023-05-05 23:39:43 +0545

    Adding and Retrieving Done

--- sakar/sakarapp/views.py
+++ sakar/sakarapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
+from .forms import StudentRegistration
+from .models import User
+def add_show(request):
+    if request.method == 'POST':
+        fm=StudentRegistration(request.POST)
+        if fm.is_valid():
+            fm.save()
+            fm = StudentRegistration()
+    else:
+        fm = StudentRegistration()
+    info = User.objects.all()
+    return render(request, 'sakarapp/addandshow.html' , {'form':fm , 'info':info})
